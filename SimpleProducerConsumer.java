eb3b438dfe86c54e052572049453dec3aa942aff
maxlink <max.durham.link@gmail.com>
2024-11-21 10:07:02 -0600

    handled thread termination

--- SimpleProducerConsumer.java
+++ SimpleProducerConsumer.java
@@ -4,8 +4,11 @@ public class SimpleProducerConsumer {
     static class Producer implements Runnable {
         @Override
         public void run() {
-            System.out.println("Producer is running...");
-            System.out.println("Producer produced an item!");
+            while (!Thread.currentThread().isInterrupted()) { // while lets thread run continously & checks for
+                                                              // interrupt from main
+                System.out.println("Producer is running...");
+                System.out.println("Producer produced an item!");
+            }
         }
     }
 
@@ -13,34 +16,40 @@ public class SimpleProducerConsumer {
     static class Consumer implements Runnable {
         @Override
         public void run() {
-            System.out.println("Consumer is running...");
-            System.out.println("Consumer consumed an item!");
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("Consumer is running...");
+                System.out.println("Consumer consumed an item!");
+            }
         }
     }
 
     public static void main(String[] args) {
 
-        //initialize buffer 
+        // initialize buffer
         // fixed size buffer of size 5
-        int[] buffer = new int[5]; 
+        int[] buffer = new int[5];
 
-        
         // Create and start the producer thread
-        Thread producerThread = new Thread(new Producer()); // makes producer 
+        Thread producerThread = new Thread(new Producer()); // makes producer
         producerThread.start();
 
         // Create and start the consumer thread
-        Thread consumerThread = new Thread(new Consumer()); // makes consumer 
+        Thread consumerThread = new Thread(new Consumer()); // makes consumer
         consumerThread.start();
 
         // Wait for threads to finish execution
-        try { // TODO - why is there a join here 
-            producerThread.join();
-            consumerThread.join();
-        } catch (InterruptedException e) { // if the join does not work, do interrupt 
+        try { // joins make main wait until threads done, but we are replacing with sleep
+              // producerThread.join();
+              // consumerThread.join();
+            Thread.sleep(5000); // main function is sleeping for 5000 milliseconds
+        } catch (InterruptedException e) { // if the join does not work, do interrupt
             Thread.currentThread().interrupt();
         }
 
-        System.out.println("Both producer and consumer have completed their tasks."); // alert that threads are done 
+        // interrupt
+        producerThread.interrupt();
+        consumerThread.interrupt();
+
+        System.out.println("Both producer and consumer have completed their tasks."); // alert that threads are done
     }
 }
